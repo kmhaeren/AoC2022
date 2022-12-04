a1455fc9d71462ac02166ef1eac27b7c1cf9ad55
Karel Haerens <karel.haerens@ml6.eu>
2022-12-04 16:28:03 +0100

    typo

--- day4/main.py
+++ day4/main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import re
-
 with open("day4/input.txt") as fp:
     s1, e1, s2, e2= np.array([re.search(r"(\d+)-(\d+),(\d+)-(\d+)", l).groups() for l in fp.readlines()],dtype=int).T
-
 print("part 1:", np.sum((s1 - s2) * (e1 - e2) <= 0))
-print("parts2:", np.sum((e2 - s1) * (s2 - e1) <= 0))
+print("part 2:", np.sum((e2 - s1) * (s2 - e1) <= 0))
