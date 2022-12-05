5f9f0133396609bacee583f39f5ad1a1e7dfdd23
Karel Haerens <karel.haerens@ml6.eu>
2022-12-05 22:44:30 +0100

    cleaner

--- day5/main.py
+++ day5/main.py
@@ -1,40 +1,30 @@
 import re
-import numpy as np
+import copy
 
 with open("day5/input.txt") as fp:
     lines =  [l.strip() for l in fp.readlines()]
 
-moves = []
-stacks_height = lines.index("") - 1
-instruction_start = lines.index("") + 1
+counter = 0
+stacks = []
 
-for i in range(instruction_start, len(lines)):
-    a, b, c = map(int, re.search(r"move (\d+) from (\d+) to (\d+)", lines[i]).groups())
-    moves.append([a, b-1, c-1])
+while lines[counter+1] != "":
+    line = lines[counter][1::4]
+    stacks.extend([[] for _ in range(len(line) - len(stacks))])
+    for s, l in zip(stacks, line):
+        if l != " ":
+            s.insert(0, l)
+    counter += 1
 
-stacks = [[] for _ in range(1 + len(lines[stacks_height])//4)]
+stacks2 = copy.deepcopy(stacks)
 
-for l in lines[0:stacks_height]:
-    for i, c in enumerate(l[1::4]):
-        if c != " ":
-            stacks[i] = [c] + stacks[i] 
+for line in lines[counter+2:]:
+    v, src, dst = map(int, re.search(r"move (\d+) from (\d+) to (\d+)", line).groups())
+    for _ in range(v):
+        stacks[dst-1].append(stacks[src-1].pop())
+    
+    stacks2[dst-1] += stacks2[src-1][-v:]
+    stacks2[src-1] = stacks2[src-1][:-v]
 
-for c, frm, to in moves:
-    for _ in range(c):
-        stacks[to].append(stacks[frm].pop())
 
 print("part 1:", "".join([s[-1] for s in stacks if len(s) > 0]))
-
-
-stacks = [[] for _ in range(1 + len(lines[stacks_height])//4)]
-
-for l in lines[0:stacks_height]:
-    for i, c in enumerate(l[1::4]):
-        if c != " ":
-            stacks[i] = [c] + stacks[i]
-
-for c, frm, to in moves:
-    stacks[to] += stacks[frm][-c:]
-    stacks[frm] = stacks[frm][:-c]
-
-print("part 2:", "".join([s[-1] for s in stacks if len(s) > 0]))
+print("part 2:", "".join([s[-1] for s in stacks2 if len(s) > 0]))
